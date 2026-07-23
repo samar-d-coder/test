61ee97d2f911234c8966d784bf54c4678d3a5598
samar <lyla07515@gmail.com>
2026-07-23 11:38:22 +0530

    Add solution for roman-to-integer

--- roman-to-integer/Solution.cpp
+++ roman-to-integer/Solution.cpp
@@ -1,18 +1,21 @@
-    int romanToInt(string s) {
-        unordered_map<char, int> mp = {
-            {'I', 1}, {'V', 5}, {'X', 10},
-            {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
         };
 
         int ans = 0;
 
-        for (int i = 0; i < s.size(); i++) {
-            if (i + 1 < s.size() && mp[s[i]] < mp[s[i + 1]])
-                ans -= mp[s[i]];
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && value[s[i]] < value[s[i + 1]])
+                ans -= value[s[i]];
             else
-                ans += mp[s[i]];
+                ans += value[s[i]];
         }
-
-        return ans;
-    }
-};
+            {'L', 50},
+            {'X', 10},
+            {'I', 1},
+            {'V', 5},
+        unordered_map<char, int> value = {
+    int romanToInt(string s) {
+public:
+class Solution {
